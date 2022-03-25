9bc374728f14585b1b27fdf98381de6095b383aa
Petr Šebela <petrsebel@seznam.cz>
2022-03-25 22:35:39 +0100

    -

--- app/Router/RouterFactory.php
+++ app/Router/RouterFactory.php
@@ -23,18 +23,26 @@ final class RouterFactory
         $routerAdmin[] = new Route('admin/<presenter>/<action>[/<id>]', 'Authentication:default');
 
         $router[] = $routerFront = new RouteList("Front");
+
         $routerFront[] = new Route('nabidka-mopedaut', 'Cars:default');
+
         $routerFront[] = new Route('nahradni-dily/produkt/<seoName>', 'SpareParts:detail');
         $routerFront[] = new Route('nahradni-dily[/<seoName>][/<page>]', 'SpareParts:default');
+
         $routerFront[] = new Route('kosik/osobni-udaje', 'Cart:step1');
         $routerFront[] = new Route('kosik/doprava-a-platba', 'Cart:step2');
         $routerFront[] = new Route('dokoncena-objednavka/detail/<hash>', 'Cart:step3');
         $routerFront[] = new Route('kosik/prazdny-kosik', 'Cart:empty');
+
         $routerFront[] = new Route('vykup', 'Pages:redemption');
         $routerFront[] = new Route('servis', 'Pages:service');
         $routerFront[] = new Route('o-mopedautech', 'Pages:about');
         $routerFront[] = new Route('kontakt', 'Pages:contact');
+        $routerFront[] = new Route('obchodni-podminky', 'Pages:termsAndConditions');
+        $routerFront[] = new Route('ochrana-osobnich-udaju', 'Pages:personalData');
+
         $routerFront[] = new Route('order/payment-result/<id>', 'Comgate:paymentResult');
+
         $routerFront[] = new Route('<presenter>/<action>', 'Homepage:default');
 
 		return $router;
